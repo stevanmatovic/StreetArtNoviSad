6494bf230d4737d0bcf9b15fc30067e27e38f47e
stevanmatovic <stevan.matovic95@gmail.com>
2017-12-17 10:43:33 +0100

    rest spring

---
+++ app/src/main/java/eu/execom/helenazecevic/monumentum/repository/restClients/PostRestClient.java
@@ -0,0 +1,11 @@
+package eu.execom.helenazecevic.monumentum.repository.restClients;
+
+import org.androidannotations.rest.spring.annotations.Rest;
+
+/**
+ * Created by Stevan on 17-Dec-17.
+ */
+
+@Rest
+public interface PostRestClient {
+}
